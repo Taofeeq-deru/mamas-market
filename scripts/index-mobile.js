dcb840435c2da9e9ff9c4526c8b5e319171c0f5c
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-05-14 11:21:14 +0100

    update

--- scripts/index-mobile.js
+++ scripts/index-mobile.js
@@ -1,6 +1,10 @@
 const buttonRight = document.getElementById("slideRight");
 const buttonLeft = document.getElementById("slideLeft");
 
+let secondNav = document.getElementById("second-nav");
+
+let sticky = secondNav.offsetTop;
+
 buttonRight.onclick = function () {
   document.getElementById("pills-tab").scrollLeft += 200;
 };
@@ -8,3 +12,13 @@ buttonRight.onclick = function () {
 buttonLeft.onclick = function () {
   document.getElementById("pills-tab").scrollLeft -= 200;
 };
+
+function handleStickyNav() {
+  if (window.pageYOffset >= sticky) {
+    secondNav.classList.add("sticky-top-nav");
+  } else {
+    secondNav.classList.remove("sticky-top-nav");
+  }
+}
+
+window.addEventListener("scroll", handleStickyNav);
