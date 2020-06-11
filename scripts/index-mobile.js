d5d01c19abd37a0347460884db12f48b0aa79439
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-06-11 02:02:03 +0100

    more mobile pages

--- scripts/index-mobile.js
+++ scripts/index-mobile.js
@@ -1,10 +1,6 @@
 const buttonRight = document.getElementById("slideRight");
 const buttonLeft = document.getElementById("slideLeft");
 
-let secondNav = document.getElementById("second-nav");
-
-let sticky = secondNav.offsetTop;
-
 buttonRight.onclick = function () {
   document.getElementById("pills-tab").scrollLeft += 200;
 };
@@ -13,12 +9,20 @@ buttonLeft.onclick = function () {
   document.getElementById("pills-tab").scrollLeft -= 200;
 };
 
-function handleStickyNav() {
-  if (window.pageYOffset >= sticky) {
-    secondNav.classList.add("sticky-top-nav");
-  } else {
-    secondNav.classList.remove("sticky-top-nav");
-  }
+function showAllTabContent() {
+  let allTab = document.querySelector("#pills-all");
+  let otherTabs = document.querySelectorAll(".pt-tab");
+  let tabContents = "";
+
+  otherTabs.forEach((otherTab) => {
+    let tabProduce = otherTab.querySelectorAll(".produce");
+
+    if (tabProduce.length == 0) return false;
+
+    tabContents += otherTab.innerHTML;
+  });
+
+  allTab.innerHTML = tabContents;
 }
 
-window.addEventListener("scroll", handleStickyNav);
+window.addEventListener("load", showAllTabContent);
