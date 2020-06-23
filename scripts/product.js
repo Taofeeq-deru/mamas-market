ef903d43100739f5a4eaa0471ac45bc68339f333
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-06-23 23:38:55 +0100

    image resizing

--- scripts/product.js
+++ scripts/product.js
@@ -2,6 +2,23 @@ let head1 = document.getElementById("head1");
 let head2 = document.getElementById("head2");
 let head3 = document.getElementById("head3");
 
+document.querySelectorAll(".picture").forEach(function (image) {
+  let src = image.getAttribute("src");
+
+  let img_replacement = document.createElement("div");
+  img_replacement.src = src;
+  img_replacement.classList.add("order-1", "picture-dimensions");
+  img_replacement.style.backgroundSize = "auto 100%";
+  img_replacement.style.backgroundRepeat = "no-repeat";
+  img_replacement.style.backgroundPosition = "top";
+  img_replacement.style.backgroundImage = "url(" + src + ")";
+  image.parentElement
+    .querySelector(".name-star-amount")
+    .classList.add("order-2");
+  image.parentNode.appendChild(img_replacement);
+  image.style.display = "none";
+});
+
 function myFunction() {
   head1.classList.add("active");
 }
